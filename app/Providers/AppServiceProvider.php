388ea9af29feb4d1ae8f2bdb4c791417dd8686e2
Renan Rodrigues <renan_rsantos@unidavi.edu.br>
2017-10-26 12:49:49 -0200

    remover https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,9 +13,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if(env('APP_ENV') === 'production'){
-            $this->app['request']->server->set('HTTPS', true);
-        }
+//        if(env('APP_ENV') === 'production'){
+//            $this->app['request']->server->set('HTTPS', true);
+//        }
     }
 
     /**
