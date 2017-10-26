5a907f15a2b02988d6090daa199b0e8a129a3305
Renan Rodrigues <renan_rsantos@unidavi.edu.br>
2017-10-25 22:58:28 -0200

    https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,9 +14,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if (App::environment('production')) {
-            URL::forceScheme('https');
-        }
+        URL::forceScheme('https');
     }
 
     /**
