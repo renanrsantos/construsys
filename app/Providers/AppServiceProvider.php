fa799a6d51df52b9fec7298b472ccf2c647e5211
Renan Rodrigues <renan_rsantos@unidavi.edu.br>
2017-10-25 22:52:00 -0200

    procfile

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        if (App::environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 
     /**
